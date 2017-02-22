f2e2ccdc8d4fc48bddd8d88da21df3f816b9fab7
leesi1595 <leesi1595@163.com>
2017-02-23 00:32:43 +0800

    2/23

---
+++ 2015_spring/task/task0002/myCode/until.js
@@ -0,0 +1,90 @@
+// 判断arr是否为一个数组，返回一个bool值
+function isArray(arr) {
+    return Object.protopype.toString.call(arr)===[object Array];
+}
+
+// 判断fn是否为一个函数，返回一个bool值
+function isFunction(fn) {
+    return Object.protopype.toString.call(fn)===[object function];
+}
+
+/**
+ * 判断一个对象是不是字面量对象，即判断这个对象是不是由{}或者new Object类似方式创建
+ *
+ * 事实上来说，在Javascript语言中，任何判断都一定会有漏洞，因此本方法只针对一些最常用的情况进行了判断
+ *
+ * @returns {Boolean} 检查结果
+ */
+function isPlain(obj){
+    var hasOwnProperty = Object.prototype.hasOwnProperty,
+        key;
+    if ( !obj ||
+         //一般的情况，直接用toString判断
+         Object.prototype.toString.call(obj) !== "[object Object]" ||
+         //IE下，window/document/document.body/HTMLElement/HTMLCollection/NodeList等DOM对象上一个语句为true
+         //isPrototypeOf挂在Object.prototype上的，因此所有的字面量都应该会有这个属性
+         //对于在window上挂了isPrototypeOf属性的情况，直接忽略不考虑
+         !('isPrototypeOf' in obj)
+       ) {
+        return false;
+    }
+
+    //判断new fun()自定义对象的情况
+    //constructor不是继承自原型链的
+    //并且原型中有isPrototypeOf方法才是Object
+    if ( obj.constructor &&
+        !hasOwnProperty.call(obj, "constructor") &&
+        !hasOwnProperty.call(obj.constructor.prototype, "isPrototypeOf") ) {
+        return false;
+    }
+    //判断有继承的情况
+    //如果有一项是继承过来的，那么一定不是字面量Object
+    //OwnProperty会首先被遍历，为了加速遍历过程，直接看最后一项
+    for ( key in obj ) {}
+    return key === undefined || hasOwnProperty.call( obj, key );
+}
+
+
+// 使用递归来实现一个深度克隆，可以复制一个目标对象，返回一个完整拷贝
+// 被复制的对象类型会被限制为数字、字符串、布尔、日期、数组、Object对象。不会包含函数、正则对象等
+function cloneObject(src) {
+    var result=src,i,len;
+    if (!src
+    	||src instanceof Number
+    	||src instanceof String
+    	||src instanceof Boolean) {
+    	return result;
+    }else if (isArray(src)) {
+    	result=[];
+    	for(var i=0;i<src.length;i++){
+    		result[i]=cloneObject(src[i]);
+    	}
+    }else if (isPlain(src)) {
+    	result={};
+    	for(i in src){
+    		if (src.hasOwnProperty(i)) {
+                result[i] = cloneObject(source[i]);
+            }
+    	}
+    }
+    return result;
+}
+// 测试用例：
+var srcObj = {
+    a: 1,
+    b: {
+        b1: ["hello", "hi"],
+        b2: "JavaScript"
+    }
+};
+var abObj = srcObj;
+var tarObj = cloneObject(srcObj);
+
+srcObj.a = 2;
+srcObj.b.b1[0] = "Hello";
+
+console.log(abObj.a);
+console.log(abObj.b.b1[0]);
+
+console.log(tarObj.a);      // 1
+console.log(tarObj.b.b1[0]);    // "hello"
